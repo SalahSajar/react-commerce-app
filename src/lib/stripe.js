d464438541257e479fbd77bae43f944366c0df11
Salah Sajar <sajarsalaheldine@gmail.com>
2022-01-14 17:49:30 +0100

    third time trying to deploy by fixing some warning not even errors

--- src/lib/stripe.js
+++ src/lib/stripe.js
@@ -1,5 +1,5 @@
 import {loadStripe} from '@stripe/stripe-js';
-import {Elements,ElementsConsumer, CardElement} from '@stripe/react-stripe-js';
+import {Elements,ElementsConsumer} from '@stripe/react-stripe-js';
 
 const stripePromise = loadStripe('pk_test_51J7LH8K2J9IcJX9wqutIuVZ8iOQN0ToEyxSYtke5U2k1MGaWc1EufaUXa38Rv7Ru6xRC33254nqoQaAmKOdcfcOI00vlccXNVn');
 
